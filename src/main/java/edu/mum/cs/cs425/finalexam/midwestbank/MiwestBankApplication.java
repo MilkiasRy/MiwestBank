ca9db7e13692ab266b94b31b013cf6cf67ddcdde
MilkiasRy <milkiasghebremariam@gmail.com>
2019-08-12 17:03:33 -0500

    'intial'

---
+++ src/main/java/edu/mum/cs/cs425/finalexam/midwestbank/MiwestBankApplication.java
@@ -0,0 +1,14 @@
+package edu.mum.cs.cs425.finalexam.midwestbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiwestBankApplication {
+
+	public static void main(String[] args) {
+		String fort=new String("milkias").toUpperCase();
+		System.out.println(fort);
+	}
+
+}
